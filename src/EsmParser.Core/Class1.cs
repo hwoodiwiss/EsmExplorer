e1d411bc3d873be01e772f5d00f66d0df5a426b8
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2026-07-28 21:28:40 +0100

    Initial commit

---
+++ src/EsmParser.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace EsmParser.Core;
+
+public class Class1
+{
+
+}
---
+++ tests/EsmParser.Core.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace EsmParser.Core.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
